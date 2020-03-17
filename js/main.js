4ec13e04869b77c9a8057ab183f42e0cf6f69a8e
Thibault Béziers la Fosse <thiblf@gmail.com>
2020-03-17 21:16:27 +0100

    added skeleton and data

---
+++ js/main.js
@@ -0,0 +1,53 @@
+new Vue({
+    el: '#app',
+    data: {
+        menu : 'home',
+        modifiers : null,
+        abilities : null
+    },
+    methods: {
+        set: function (param) {
+            this.menu = param
+        },
+        loadDatabase: function() {
+            this.modifiers = []
+            this.abilities = []
+            modifiers = database.attack_modifiers 
+            abilities = database.characters_abilities
+            currentId = null
+            modifier = null
+            modifiers.forEach(elem => {
+                id = elem.name.substring(0,5)
+                if (id.endsWith('-')) {
+                    id = elem.name.substring(0,4)
+                }
+                if (id != currentId) {                
+                    if ( modifier != null) {
+                        this.modifiers.push(modifier)
+                    }
+
+                    currentId = id           
+                    modifier = {name : id, cards : []}                    
+                }
+                modifier.cards.push(elem)
+            })
+            character = null
+            abilities.forEach(elem => {
+                if(elem.name.endsWith('-back')) {                    
+                    if (character != null) {
+                        this.abilities.push(character)
+                    }
+                    character = {name: '', abilities: []}         
+                    character.name = elem.name.substring(0,2)             
+                    character.abilities = []                                                             
+                }
+                character.abilities.push(elem)
+            })
+        }
+    }, 
+    beforeMount(){
+        this.loadDatabase()
+        console.log(JSON.stringify(this.modifiers))
+        console.log(JSON.stringify(this.abilities))
+    }
+  })
