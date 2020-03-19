3e81f40a2f5ca7af0de146cc54a6002bc923b7a6
Raphael Page <page.raf@gmail.com>
2020-03-19 21:38:21 +0100

    Display both base modifier deck and category modifier deck

--- js/main.js
+++ js/main.js
@@ -73,6 +73,13 @@ new Vue({
             this.abilities.push(character)
 
         },
+        addModifier: function(card) {   
+            this.modifiersChosen.push(card)
+        },
+        removeModifier: function(card) {
+            indexOfCardToRemove = this.abilitiesChosen.indexOf(card)
+            this.modifiersChosen.splice(indexOfCardToRemove, 1)
+        },
         addAbility: function(card) {   
             card.duration = 0
             this.abilitiesChosen.push(card)
