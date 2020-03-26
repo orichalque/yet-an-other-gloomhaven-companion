dc1eb143d308a734662b55d8b665a2d4c7284add
Raphaël Pagé <raphael.page@obeo.fr>
2020-03-26 16:36:03 +0100

    Cleanup SW

--- js/database.js
+++ js/database.js
@@ -2074,31 +2074,31 @@ database = {
     {
       "name": "am-sw-02",
       "points": 379,
-      "image": "attack-modifiers/SW/am-sw-02.png",
+      "image": "attack-modifiers/SW/am-sw-01.png",
       "xws": "amsw02"
     },
     {
       "name": "am-sw-03",
       "points": 380,
-      "image": "attack-modifiers/SW/am-sw-03.png",
+      "image": "attack-modifiers/SW/am-sw-01.png",
       "xws": "amsw03"
     },
     {
       "name": "am-sw-04",
       "points": 381,
-      "image": "attack-modifiers/SW/am-sw-04.png",
+      "image": "attack-modifiers/SW/am-sw-01.png",
       "xws": "amsw04"
     },
     {
       "name": "am-sw-05",
       "points": 382,
-      "image": "attack-modifiers/SW/am-sw-05.png",
+      "image": "attack-modifiers/SW/am-sw-01.png",
       "xws": "amsw05"
     },
     {
       "name": "am-sw-06",
       "points": 383,
-      "image": "attack-modifiers/SW/am-sw-06.png",
+      "image": "attack-modifiers/SW/am-sw-01.png",
       "xws": "amsw06"
     },
     {
@@ -2134,7 +2134,7 @@ database = {
     {
       "name": "am-sw-12",
       "points": 389,
-      "image": "attack-modifiers/SW/am-sw-12.png",
+      "image": "attack-modifiers/SW/am-sw-11.png",
       "xws": "amsw12"
     },
     {
@@ -2146,7 +2146,7 @@ database = {
     {
       "name": "am-sw-14",
       "points": 391,
-      "image": "attack-modifiers/SW/am-sw-14.png",
+      "image": "attack-modifiers/SW/am-sw-13.png",
       "xws": "amsw14"
     },
     {
