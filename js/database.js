ab965633030069aca7e7d93495eda3fef83ad1e4
Raphaël Pagé <raphael.page@obeo.fr>
2020-03-26 13:33:11 +0100

    Cleanup SC

--- js/database.js
+++ js/database.js
@@ -1642,7 +1642,7 @@ database = {
     {
       "name": "am-sc-04",
       "points": 307,
-      "image": "attack-modifiers/SC/am-sc-04.png",
+      "image": "attack-modifiers/SC/am-sc-03.png",
       "xws": "amsc04"
     },
     {
@@ -1654,19 +1654,19 @@ database = {
     {
       "name": "am-sc-06",
       "points": 309,
-      "image": "attack-modifiers/SC/am-sc-06.png",
+      "image": "attack-modifiers/SC/am-sc-05.png",
       "xws": "amsc06"
     },
     {
       "name": "am-sc-07",
       "points": 310,
-      "image": "attack-modifiers/SC/am-sc-07.png",
+      "image": "attack-modifiers/SC/am-sc-05.png",
       "xws": "amsc07"
     },
     {
       "name": "am-sc-08",
       "points": 311,
-      "image": "attack-modifiers/SC/am-sc-08.png",
+      "image": "attack-modifiers/SC/am-sc-05.png",
       "xws": "amsc08"
     },
     {
@@ -1678,7 +1678,7 @@ database = {
     {
       "name": "am-sc-10",
       "points": 313,
-      "image": "attack-modifiers/SC/am-sc-10.png",
+      "image": "attack-modifiers/SC/am-sc-09.png",
       "xws": "amsc10"
     },
     {
@@ -1690,19 +1690,19 @@ database = {
     {
       "name": "am-sc-12",
       "points": 315,
-      "image": "attack-modifiers/SC/am-sc-12.png",
+      "image": "attack-modifiers/SC/am-sc-11.png",
       "xws": "amsc12"
     },
     {
       "name": "am-sc-13",
       "points": 316,
-      "image": "attack-modifiers/SC/am-sc-13.png",
+      "image": "attack-modifiers/SC/am-sc-11.png",
       "xws": "amsc13"
     },
     {
       "name": "am-sc-14",
       "points": 317,
-      "image": "attack-modifiers/SC/am-sc-14.png",
+      "image": "attack-modifiers/SC/am-sc-11.png",
       "xws": "amsc14"
     },
     {
@@ -1714,7 +1714,7 @@ database = {
     {
       "name": "am-sc-16",
       "points": 319,
-      "image": "attack-modifiers/SC/am-sc-16.png",
+      "image": "attack-modifiers/SC/am-sc-15.png",
       "xws": "amsc16"
     },
     {
