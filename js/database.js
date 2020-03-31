877958a810310c4a1053ccac354160247579b25e
Thibault Béziers la Fosse <thiblf@gmail.com>
2020-03-31 13:26:37 +0200

    Added spoiler data at the end of the js database

--- js/database.js
+++ js/database.js
@@ -7705,3 +7705,382 @@ allItems = [
         ]
     }
 ]
+
+// SPOILER SECTION 
+
+XEnvelopeModifiers = {
+    "name": "am-bs",
+    "cards": [
+       {
+          "name": "am-bs-01",
+          "points": 122,
+          "image": "attack-modifiers/BS/am-bs-01.png",
+          "xws": "ambs01"
+       },
+       {
+          "name": "am-bs-02",
+          "points": 123,
+          "image": "attack-modifiers/BS/am-bs-02.png",
+          "xws": "ambs02"
+       },
+       {
+          "name": "am-bs-05",
+          "points": 126,
+          "image": "attack-modifiers/BS/am-bs-02.png",
+          "xws": "ambs05"
+       },
+       {
+          "name": "am-bs-08",
+          "points": 129,
+          "image": "attack-modifiers/BS/am-bs-02.png",
+          "xws": "ambs08"
+       },
+       {
+          "name": "am-bs-12",
+          "points": 133,
+          "image": "attack-modifiers/BS/am-bs-02.png",
+          "xws": "ambs12"
+       },
+       {
+          "name": "am-bs-03",
+          "points": 124,
+          "image": "attack-modifiers/BS/am-bs-03.png",
+          "xws": "ambs03"
+       },
+       {
+          "name": "am-bs-10",
+          "points": 131,
+          "image": "attack-modifiers/BS/am-bs-03.png",
+          "xws": "ambs10"
+       },
+       {
+          "name": "am-bs-04",
+          "points": 125,
+          "image": "attack-modifiers/BS/am-bs-04.png",
+          "xws": "ambs04"
+       },
+       {
+          "name": "am-bs-06",
+          "points": 127,
+          "image": "attack-modifiers/BS/am-bs-06.png",
+          "xws": "ambs06"
+       },
+       {
+          "name": "am-bs-09",
+          "points": 130,
+          "image": "attack-modifiers/BS/am-bs-06.png",
+          "xws": "ambs09"
+       },
+       {
+          "name": "am-bs-07",
+          "points": 128,
+          "image": "attack-modifiers/BS/am-bs-07.png",
+          "xws": "ambs07"
+       },
+       {
+          "name": "am-bs-11",
+          "points": 132,
+          "image": "attack-modifiers/BS/am-bs-11.png",
+          "xws": "ambs11"
+       },
+       {
+          "name": "am-bs-13",
+          "points": 134,
+          "image": "attack-modifiers/BS/am-bs-11.png",
+          "xws": "ambs13"
+       },
+       {
+          "name": "am-bs-14",
+          "points": 135,
+          "image": "attack-modifiers/BS/am-bs-14.png",
+          "xws": "ambs14"
+       },
+       {
+          "name": "am-bs-15",
+          "points": 136,
+          "image": "attack-modifiers/BS/am-bs-15.png",
+          "xws": "ambs15"
+       }
+    ]
+ }
+
+ XEnvelopeAbilities = {
+    "name": "bs",
+    "max" : 11,
+    "hidden" : true,
+    "cards": [
+        {
+            "name": "bs-back",
+            "points": 60,
+            "image": "character-ability-cards/BS/bs-back.png",
+            "xws": "bsback",
+            "level": 0
+        },
+        {
+            "name": "bioluminescence",
+            "points": 61,
+            "image": "character-ability-cards/BS/bioluminescence.png",
+            "xws": "bioluminescence",
+            "level": 5
+        },
+        {
+            "name": "blood drain",
+            "points": 62,
+            "image": "character-ability-cards/BS/blood-drain.png",
+            "xws": "blooddrain",
+            "level": 8
+        },
+        {
+            "name": "bone daggers",
+            "points": 63,
+            "image": "character-ability-cards/BS/bone-daggers.png",
+            "xws": "bonedaggers",
+            "level": 5
+        },
+        {
+            "name": "call of the grave",
+            "points": 64,
+            "image": "character-ability-cards/BS/call-of-the-grave.png",
+            "xws": "callofthegrave",
+            "level": 1
+        },
+        {
+            "name": "corrupting parasites",
+            "points": 65,
+            "image": "character-ability-cards/BS/corrupting-parasites.png",
+            "xws": "corruptingparasites",
+            "level": 1
+        },
+        {
+            "name": "death march",
+            "points": 66,
+            "image": "character-ability-cards/BS/death-march.png",
+            "xws": "deathmarch",
+            "level": 1
+        },
+        {
+            "name": "deflecting blades",
+            "points": 67,
+            "image": "character-ability-cards/BS/deflecting-blades.png",
+            "xws": "deflectingblades",
+            "level": 1
+        },
+        {
+            "name": "engulfing stingers",
+            "points": 68,
+            "image": "character-ability-cards/BS/engulfing-stingers.png",
+            "xws": "engulfingstingers",
+            "level": 1
+        },
+        {
+            "name": "erosion",
+            "points": 69,
+            "image": "character-ability-cards/BS/erosion.png",
+            "xws": "erosion",
+            "level": 6
+        },
+        {
+            "name": "focused scourge",
+            "points": 70,
+            "image": "character-ability-cards/BS/focused-scourge.png",
+            "xws": "focusedscourge",
+            "level": 8
+        },
+        {
+            "name": "grasping advance",
+            "points": 71,
+            "image": "character-ability-cards/BS/grasping-advance.png",
+            "xws": "graspingadvance",
+            "level": 0
+        },
+        {
+            "name": "hive mind",
+            "points": 72,
+            "image": "character-ability-cards/BS/hive-mind.png",
+            "xws": "hivemind",
+            "level": 1
+        },
+        {
+            "name": "incubation",
+            "points": 73,
+            "image": "character-ability-cards/BS/incubation.png",
+            "xws": "incubation",
+            "level": 4
+        },
+        {
+            "name": "infest",
+            "points": 74,
+            "image": "character-ability-cards/BS/infest.png",
+            "xws": "infest",
+            "level": 2
+        },
+        {
+            "name": "oasis",
+            "points": 75,
+            "image": "character-ability-cards/BS/oasis.png",
+            "xws": "oasis",
+            "level": 9
+        },
+        {
+            "name": "omniscient assault",
+            "points": 76,
+            "image": "character-ability-cards/BS/omniscient-assault.png",
+            "xws": "omniscientassault",
+            "level": 1
+        },
+        {
+            "name": "prismatic cyclone",
+            "points": 77,
+            "image": "character-ability-cards/BS/prismatic-cyclone.png",
+            "xws": "prismaticcyclone",
+            "level": 6
+        },
+        {
+            "name": "putrid grubs",
+            "points": 78,
+            "image": "character-ability-cards/BS/putrid-grubs.png",
+            "xws": "putridgrubs",
+            "level": 3
+        },
+        {
+            "name": "sand scythe",
+            "points": 79,
+            "image": "character-ability-cards/BS/sand-scythe.png",
+            "xws": "sandscythe",
+            "level": 3
+        },
+        {
+            "name": "scattered defense",
+            "points": 80,
+            "image": "character-ability-cards/BS/scattered-defense.png",
+            "xws": "scattereddefense",
+            "level": 1
+        },
+        {
+            "name": "solitary horde",
+            "points": 81,
+            "image": "character-ability-cards/BS/solitary-horde.png",
+            "xws": "solitaryhorde",
+            "level": 7
+        },
+        {
+            "name": "sunstroke",
+            "points": 82,
+            "image": "character-ability-cards/BS/sunstroke.png",
+            "xws": "sunstroke",
+            "level": 7
+        },
+        {
+            "name": "swarming minions",
+            "points": 83,
+            "image": "character-ability-cards/BS/swarming-minions.png",
+            "xws": "swarmingminions",
+            "level": 1
+        },
+        {
+            "name": "sword of tenacity",
+            "points": 84,
+            "image": "character-ability-cards/BS/sword-of-tenacity.png",
+            "xws": "swordoftenacity",
+            "level": 4
+        },
+        {
+            "name": "the storm's edge",
+            "points": 85,
+            "image": "character-ability-cards/BS/the-storms-edge.png",
+            "xws": "thestormsedge",
+            "level": 1
+        },
+        {
+            "name": "tomb of the immortal",
+            "points": 86,
+            "image": "character-ability-cards/BS/tomb-of-the-immortal.png",
+            "xws": "tomboftheimmortal",
+            "level": 0
+        },
+        {
+            "name": "unstoppable army",
+            "points": 87,
+            "image": "character-ability-cards/BS/unstoppable-army.png",
+            "xws": "unstoppablearmy",
+            "level": 0
+        },
+        {
+            "name": "vampiric tempest",
+            "points": 88,
+            "image": "character-ability-cards/BS/vampiric-tempest.png",
+            "xws": "vampirictempest",
+            "level": 2
+        },
+        {
+            "name": "venomous barbs",
+            "points": 89,
+            "image": "character-ability-cards/BS/venomous-barbs.png",
+            "xws": "venomousbarbs",
+            "level": 1
+        },
+        {
+            "name": "wasteland",
+            "points": 90,
+            "image": "character-ability-cards/BS/wasteland.png",
+            "xws": "wasteland",
+            "level": 9
+        },
+        {
+            "name": "lightning moths",
+            "points": 574,
+            "image": "character-ability-cards/BS/bioluminescence.png",
+            "xws": "bioluminescence",
+            "level": 5
+        },
+        {
+            "name": "soul leeches",
+            "points": 575,
+            "image": "character-ability-cards/BS/corrupting-parasites.png",
+            "xws": "corruptingparasites",
+            "level": 1
+        },
+        {
+            "name": "creeping beetles",
+            "points": 576,
+            "image": "character-ability-cards/BS/death-march.png",
+            "xws": "deathmarch",
+            "level": 1
+        },
+        {
+            "name": "angry wasps",
+            "points": 577,
+            "image": "character-ability-cards/BS/engulfing-stingers.png",
+            "xws": "engulfingstingers",
+            "level": 1
+        },
+        {
+            "name": "bloat maggots",
+            "points": 578,
+            "image": "character-ability-cards/BS/putrid-grubs.png",
+            "xws": "putridgrubs",
+            "level": 3
+        },
+        {
+            "name": "dueling hornets",
+            "points": 579,
+            "image": "character-ability-cards/BS/solitary-horde.png",
+            "xws": "solitaryhorde",
+            "level": 7
+        },
+        {
+            "name": "steel scarabs",
+            "points": 580,
+            "image": "character-ability-cards/BS/unstoppable-army.png",
+            "xws": "unstoppablearmy",
+            "level": 0
+        },
+        {
+            "name": "rust vermin",
+            "points": 581,
+            "image": "character-ability-cards/BS/wasteland.png",
+            "xws": "wasteland",
+            "level": 9
+        }
+    ]
+}
