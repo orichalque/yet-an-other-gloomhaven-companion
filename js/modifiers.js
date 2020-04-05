58e120a5b19abae89d950d7d54b311c79fc68758
Raphaël Pagé <raphael.page@obeo.fr>
2020-04-05 16:08:12 +0200

    Remove curse and blessings modifiers once drawn

--- js/modifiers.js
+++ js/modifiers.js
@@ -30,8 +30,9 @@ var modifiersManagement = {
             }            
         },
         removeModifier: function(card) {
-            indexOfCardToRemove = this.modifiersChosen.indexOf(card)
+            var indexOfCardToRemove = this.modifiersChosen.indexOf(card)
             this.modifiersChosen.splice(indexOfCardToRemove, 1)
+            var indexOfCardToRemove = this.modifiersDrawPile.indexOf(card)
             this.modifiersDrawPile.splice(indexOfCardToRemove, 1)
         },
         drawModifier: function() {
@@ -40,7 +41,17 @@ var modifiersManagement = {
                 this.modifiersDiscardPile.unshift(this.lastDrawnModifier)
             }
             this.lastDrawnModifier = this.modifiersDrawPile[randomint]
-            this.modifiersDrawPile.splice(randomint,1)
+            if(this.checkIfCurseOrBless(this.lastDrawnModifier)){
+                this.removeModifier(this.lastDrawnModifier)
+            } else {
+                this.modifiersDrawPile.splice(randomint,1)
+            }
+        },
+        checkIfCurseOrBless: function(card){
+            var blessDeck = this.modifiersSpecial.find(element => element.name == 'bless')
+            var curseDeck = this.modifiersSpecial.find(element => element.name == 'curse')
+            return ( (blessDeck !== undefined) && (blessDeck.cards.includes(card)) ) ||
+            ( (curseDeck !== undefined) && (curseDeck.cards.includes(card)) )
         },
         shuffleModifiersDeck: function() {
             this.modifiersDrawPile = this.modifiersChosen.slice()
