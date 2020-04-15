642061c59058343515ae22e3d9c3623b78d9795e
Thibault Béziers la Fosse <thiblf@gmail.com>
2020-04-15 18:10:40 +0200

    Can now overlay buffs on abilities

--- js/enhancements.js
+++ js/enhancements.js
@@ -17,9 +17,10 @@ var enhancementManagement = {
         addEnhancement : function(enhancement) {
             if (this.enhancementMode === 'top') {
                 if (this.cardToEnhance.top == null) {
-                    this.cardToEnhance.top = []                    
+                    this.cardToEnhance.top = []                                        
                 }
                 this.cardToEnhance.top.push(enhancement)
+                console.log(JSON.stringify(this.cardToEnhance))
             } 
             if (this.enhancementMode === 'bottom') {
                 if (this.cardToEnhance.bottom == null) {
@@ -27,6 +28,25 @@ var enhancementManagement = {
                 }
                 this.cardToEnhance.bottom.push(enhancement)
             }        
+            this.$forceUpdate()
+
+        },
+        removeEnhancement: function(enhancement) {
+            if (this.cardToEnhance.top != null) {
+                var indexOfEnhancementToRemove = this.cardToEnhance.top.indexOf(enhancement)
+                if (indexOfEnhancementToRemove >= 0) {
+                    this.cardToEnhance.top.splice(indexOfEnhancementToRemove,1)
+                }
+            }
+            
+            if (this.cardToEnhance.bottom != null) {
+                indexOfEnhancementToRemove = this.cardToEnhance.bottom.indexOf(enhancement)
+                if (indexOfEnhancementToRemove >= 0) {
+                    this.cardToEnhance.bottom.splice(indexOfEnhancementToRemove,1)
+                }
+            }          
+            this.$forceUpdate()
+  
         },
         setTopEnhancement : function(enhancement) {
             if (this.enhancementMode != 'Top') {
