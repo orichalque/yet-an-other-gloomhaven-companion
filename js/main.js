a3bca56f311f7879ac463f5353b941e3878856f7
Raphael Page <page.raf@gmail.com>
2021-02-27 09:33:56 +0100

    Fix New Game

--- js/main.js
+++ js/main.js
@@ -110,13 +110,14 @@ new Vue({
             this.hasEnabledCardExchange = !this.hasEnabledCardExchange 
         },
         newGame: function() {
+            this.cardsInHand = []
             this.abilitiesChosen.forEach(card => {
                 card.duration = 0
+                this.cardsInHand.push(card)
             })
             this.cardsOnBoard = []
             this.cardsDiscarded = []
             this.cardsDestroyed = []
-            this.cardsInHand = this.abilitiesChosen
 
             this.gearChosen.forEach(item => {
                 this.restoreItem(item);
@@ -183,7 +184,6 @@ new Vue({
                                     card.bottom = ability.bottom
                                 }
                                 this.abilitiesChosen.push(card)
-                                this.cardsInHand.push(card)
                             }
                         })
                     })
