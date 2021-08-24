8828acc9e6648c57e1b7f326388aad963683c24c
Rob Hess <robwhess@gmail.com>
2021-08-23 21:06:05 -0700

    Save and load battle goals gameplay data.

--- js/battlegoals.js
+++ js/battlegoals.js
@@ -4,9 +4,50 @@ var battleGoalsManagement = {
         battleGoals : [],
         battleGoalsDrawn : [],
         battleGoalPicked : [],
-        goalCounter : 0  
+        goalCounter : 0
     },
-    methods: { 
+    methods: {
+        saveBattleGoalsGameplayData: function () {
+            Cookies.set("battleGoalsDrawn", JSON.stringify(this.battleGoalsDrawn), { expires: 365 })
+            Cookies.set("battleGoalPicked", JSON.stringify(this.battleGoalPicked), { expires: 365 })
+            Cookies.set("goalCounter", JSON.stringify(this.goalCounter), { expires: 365 })
+        },
+        loadBattleGoalsGameplayData: function () {
+            // Load drawn battle goals
+            battleGoalsDrawnCookie = Cookies.get('battleGoalsDrawn')
+            if (battleGoalsDrawnCookie != null) {
+                this.battleGoalsDrawn = []
+                oldBattleGoalsDrawn = JSON.parse(battleGoalsDrawnCookie)
+                oldBattleGoalsDrawn.forEach(drawnGoal => {
+                    this.battleGoals.forEach(goal => {
+                        if (goal.name === drawnGoal.name) {
+                            this.battleGoalsDrawn.push(goal)
+                        }
+                    })
+                })
+            }
+
+            // Load picked battle goals
+            battleGoalPickedCookie = Cookies.get('battleGoalPicked')
+            if (battleGoalPickedCookie != null) {
+                this.battleGoalPicked = []
+                oldBattleGoalPicked = JSON.parse(battleGoalPickedCookie)
+                oldBattleGoalPicked.forEach(pickedGoal => {
+                    this.battleGoals.forEach(goal => {
+                        if (goal.name === pickedGoal.name) {
+                            this.battleGoalPicked.push(goal)
+                        }
+                    })
+                })
+            }
+
+            // Load battle goal counter
+            goalCounterCookie = Cookies.get("goalCounter")
+            if (goalCounterCookie != null)
+                this.goalCounter = JSON.parse(goalCounterCookie)
+
+            this.$forceUpdate()
+        },
         drawBattleGoals: function() {
             this.battleGoalsDrawn = []
             this.battleGoalPicked = []
@@ -29,7 +70,7 @@ var battleGoalsManagement = {
         incrementGoalCounter: function(){
             this.goalCounter+=1;
         },
-        resetBattlegoals: function() {            
+        resetBattlegoals: function() {
             this.battleGoalsDrawn = []
             this.battleGoalPicked = []
             this.goalCounter = 0
